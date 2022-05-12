408409399d83cfd804bc443918c3b6f0b8d81652
José Juan Téllez Guzmán <jjtellezg@gmail.com>
2022-05-12 17:32:28 -0500

    product item details

--- src/utils/sanityClient/index.js
+++ src/utils/sanityClient/index.js
@@ -1,8 +1,10 @@
-import client from "@sanity/client"
+import sanityClient from "@sanity/client"
 
-export default client({
+const client = sanityClient({
 	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
 	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
 	apiVersion: "2021-08-31", // use a UTC date string
 	useCdn: true,
 })
+
+export default client
